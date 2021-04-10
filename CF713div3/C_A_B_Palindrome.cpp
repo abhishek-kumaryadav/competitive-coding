9eb58488337b632b63449cab33873b0dd583e24a
Abhishek Kumar Yadav <abhk943@gmail.com>
2021-04-10 21:57:12 +0530

    CF713div3 2 q

---
+++ CF713div3/C_A_B_Palindrome.cpp
@@ -0,0 +1,127 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> p32;
+typedef pair<ll, ll> p64;
+typedef pair<double, double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int>> vv32;
+typedef vector<vector<ll>> vv64;
+typedef vector<vector<p64>> vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+#define ln "\n"
+#define dbg(x) cout << #x << " = " << x << ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+void solve()
+{
+}
+int main()
+{
+    fast_cin();
+    ll t;
+    cin >> t;
+    for (int it = 1; it <= t; it++)
+    {
+        int a, b;
+        cin >> a >> b;
+        string s;
+        cin >> s;
+        int n = a + b;
+        int half = n / 2;
+        for (int i = 0; i < half; i++)
+        {
+            int j = n - 1 - i;
+            if (s[i] == '0' && s[j] == '0')
+            {
+                a--;
+            }
+            else if (s[i] == '1' && s[j] == '1')
+            {
+                b--;
+            }
+            else if (s[i] == '1' && s[j] == '?')
+            {
+                s[j] = '1';
+                b--;
+            }
+            else if (s[i] == '?' && s[j] == '1')
+            {
+                s[i] = '1';
+                b--;
+            }
+            else if (s[i] == '0' && s[j] == '?')
+            {
+                s[j] = '0';
+                a--;
+            }
+            else if (s[i] == '?' && s[j] == '0')
+            {
+                s[i] = '0';
+                a--;
+            }
+            else if (s[j] == '?' && s[i] == '?')
+            {
+                count++;
+            }
+        }
+        if (n % 2 == 1)
+        {
+            if (s[i] == '?')
+                count = count * 2 + 1;
+            if (a + b == count)
+        }
+        else
+        {
+            count = count * 2;
+            if (a == 0 && b == 0)
+            {
+                cout << s << ln;
+            }
+            else
+                cout << -1 << ln;
+        }
+    }
+    return 0;
+}
